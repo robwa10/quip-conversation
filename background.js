81a081690aa8185c1aea9dfb66d8d7170cc010af
Robert Hubbard <rob.c.hubbard@gmail.com>
2018-05-09 15:00:09 +0100

    Change from browser_action to page_action

--- background.js
+++ background.js
@@ -1,5 +1,17 @@
 'use strict';
 
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    chrome.declarativeContent.onPageChanged.addRules([{
+      conditions: [new chrome.declarativeContent.PageStateMatcher({
+        pageUrl: {hostContains: 'quip.com'},
+      })
+      ],
+          actions: [new chrome.declarativeContent.ShowPageAction()]
+    }]);
+  });
+});
+
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 	const url = tab.url;
 	const onQuipSite = url.indexOf('.quip.com/') > 0 ? true : false;
@@ -12,3 +24,4 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 		}
 	});
 });
+
