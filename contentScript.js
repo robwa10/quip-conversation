fde03e8447e6041216033ec20c758088fd6b9078
Robert Hubbard <rob.c.hubbard@gmail.com>
2018-10-16 17:55:16 +0100

    Quip really tried their hardest to break this extension

--- contentScript.js
+++ contentScript.js
@@ -1,9 +1,16 @@
 (function (document) {
 	'use strict';
-	const myElements = document.getElementsByClassName('document-chat-pinned');
-	const el = myElements.length >= 1 ? myElements[0] : null;
-	if (el != null) {
+	// Get the button to close the conversation
+	const buttonContainer = document.getElementsByClassName('app-nav-conversation');
+	const children = buttonContainer[0].children;
+	const button = children[0];
+	// Get the class that contains the chat
+	const myElements = document.getElementsByClassName('has-chat');
+	// If it's open length returns 1
+	const el = myElements.length >= 1 ? true : false;
+	if (el) {
 		const closeConversation = function (elem) {
+			// Get the button el that closes the convo window
 			const evt = new MouseEvent('click', {
 				bubbles: true,
 				cancelable: false,
@@ -11,6 +18,6 @@
 			});
 			elem.dispatchEvent(evt);
 		};
-		closeConversation(el);
+		closeConversation(button);
 	}
 })(document);
